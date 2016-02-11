d11efdf54aa101ebf50664374c6847c91cc4997f
cahall <123chall@gmail.com>
2016-02-11 09:39:21 -0600

    Added project files to calvin_experiments

---
+++ Client/src/com/git/cs309/mmoclient/gui/ChatMessage.java
@@ -0,0 +1,27 @@
+package com.git.cs309.mmoclient.gui;
+
+public final class ChatMessage implements Comparable<ChatMessage> {
+	private final MessageGroup group;
+	private final String message;
+	private final long time;
+
+	public ChatMessage(final MessageGroup group, final String message) {
+		this.group = group;
+		this.message = message;
+		time = System.currentTimeMillis();
+	}
+
+	// Compare them based on creation time.
+	@Override
+	public int compareTo(ChatMessage o) {
+		return (int) (o.time - time);
+	}
+
+	public MessageGroup getGroup() {
+		return group;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
